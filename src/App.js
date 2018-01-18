f099c268845e08f8781e848cde08a7c7566d361b
Steve Hunter <steve@smtahunter.com>
2018-01-19 08:57:10 +1100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render(){
+    // const headerStyle = { color: '#ff00ff', textDecoration: 'underline' }
+    return (
+      <h2 style={{color: '#ff00ff', textDecoration: 'underline'}}> Hello World Again! </h2>
+     )
+   }
+}
+
+export default App;
