b3eba0e4eb342d5a958e7c2aadea2bedd48b5ee5
Steve Hunter <steve@smtahunter.com>
2018-01-23 13:43:58 +1100

    tut

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MessageView from './messages/message-view';
 
 class App extends Component {
   render(){
     // const headerStyle = { color: '#ff00ff', textDecoration: 'underline' }
     return (
-      <h2 style={{color: '#ff00ff', textDecoration: 'underline'}}> Hello World Again! </h2>
+      <MessageView />
      )
    }
 }
 
-export default App; 
+export default App;
